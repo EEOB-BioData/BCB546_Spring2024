9a358911ec35776fa61bdf3875fafc949718714e
mbhufford <mbhufford@gmail.com>
2024-02-07 12:43:46 -0600

    updated shell script

--- course-files/Week_04/blastn.sh
+++ course-files/Week_04/blastn.sh
@@ -2,16 +2,24 @@
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=8
 #SBATCH --time=3:00:00
-#SBATCH --mail-user=<netID>@iastate.edu
+#SBATCH --mail-user=$USER@iastate.edu
 #SBATCH --mail-type=begin
 #SBATCH --mail-type=end
-#SBATCH --error=blastn1.%J.err
-#SBATCH --output=blastn1.%J.out
+#SBATCH --job-name=blast-10k
+#SBATCH --error=blastx-10k.%J.err
+#SBATCH --output=blastx10k.%J.out
 
 cd $SLURM_SUBMIT_DIR
 
+
 module load blast-plus
 
-tblastx -db /shared/class/bcb590/546x/blast/Zea_mays_dna.fa -query /shared/class/bcb590/546x/blast/zm_pep.fa -outfmt "6 qseqid sseqid pident length mismatch gapopen qstart qend qlen sstart send slen evalue frames sgi staxid" -num_threads 8 -out zm_tblastx.out &
+blastx \
+   -db /ptmp/arnstrm/bcb590/546x/blast/zm_pep.fa \
+   -query /ptmp/arnstrm/bcb590/546x/blast/Zea_mays_dna_10k.fa \
+   -outfmt "6 qseqid sseqid pident length mismatch gapopen qstart qend qlen sstart send slen evalue frames sgi staxid" \
+   -num_threads 8 \
+   -out zm_blastx_10k.out
 
-wait
+# print job's resource usage
+scontrol show job $SLURM_JOB_ID
